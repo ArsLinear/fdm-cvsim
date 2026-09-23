747117043a2339bc1c7ab67d77607b7b68d437b9
ArsLinear <git@arslinear.com>
2026-09-23 16:42:49 +0800

    重构 _fdm_sim 函数以支持浓度参数，修正大矩阵构建逻辑并保存结果到 cv_output.csv

--- cvsim.py
+++ cvsim.py
@@ -25,54 +25,75 @@ def waveform_generator(initial, switch, scan_rate):
     return waveform
 
 
-def _fdm_sim(waveform, k0, alpha, E0, c_ox, c_red, D_ox, D_red, n):
+def _fdm_sim(waveform, k0, alpha, E0, c_ox, c_red, D_ox, D_red, n, concentration):
 
     dt = SAMPLE_TIME
     dx = SAMPLE_SPACE
     num = SPACE_NUM + 1
-
-    concentration = np.zeros((num, 2))
-
-    for i in range(0, num):
-        concentration[i, 0] = c_ox
-        concentration[i, 1] = c_red
 
     diffusivity = np.array([[D_ox, 0.0], [0.0, D_red]])
     coefficient = diffusivity * dt / dx ** 2
 
     butler_volmer = np.array([np.exp(-alpha * n * F_CONST * (waveform - E0) / (R_CONST * TEMP)),
                              - np.exp((1 - alpha) * n * F_CONST * (waveform - E0) / (R_CONST * TEMP))])
-    flux = 2 * k0 * dt / dx * butler_volmer
+    flux = 2 * k0 * dt / dx * np.outer([1.0, -1.0], butler_volmer)
 
-    big_martrix = np.zeros((num, num))
+    big_martrix = np.zeros((num, num, 2, 2))
 
-    big_martrix[0, 0] = np.ones(2) + coefficient - flux
+    big_martrix[0, 0] = np.eye(2) + 2 * coefficient + flux
     big_martrix[0, 1] = -2 * coefficient
 
-    for i in range(1, num-1):
+    for i in range(1, num - 1):
         big_martrix[i, i-1] = -coefficient
-        big_martrix[i, i] = np.ones(2) + 2 * coefficient
+        big_martrix[i, i] = np.eye(2) + 2 * coefficient
         big_martrix[i, i+1] = -coefficient
 
-    big_martrix[num, num] = np.array([c_ox, 0], [0, c_red])
+    big_martrix[num-1, num-1] = np.eye(2)
 
-    concentration = np.linalg.solve(big_martrix, concentration)
+    big_martrix = big_martrix.transpose(0, 2, 1, 3).reshape(2 * num, 2 * num)
+    concentration[num-1] = [c_ox, c_red]
+    concentration = np.linalg.solve(big_martrix, concentration.reshape(-1)).reshape(num, 2)
 
     return concentration
 
 def cv_sim(initial, switch, scan_rate, k0, alpha, E0, c_ox, c_red, D_ox, D_red, n):
 
-    step = int((switch - initial) / scan_rate / SAMPLE_TIME)
-    current = np.zeros(step)
-
     waveform = waveform_generator(initial, switch, scan_rate)
+    current = np.zeros(len(waveform))
+    concentration = np.tile([c_ox, c_red], (SPACE_NUM + 1, 1))
 
     for t in range(len(waveform)):
-        concentration = _fdm_sim(waveform[t], k0, alpha, E0, c_ox, c_red, D_ox, D_red, n)
+        concentration = _fdm_sim(
+            waveform[t], k0, alpha, E0, c_ox, c_red, D_ox, D_red, n, concentration
+        )
         concentration_surface = concentration[0, :]
-        current[t] = n * F_CONST * D_red * (concentration[1, 1] - concentration[0, 1]) / SAMPLE_SPACE
         butler_volmer = np.array([np.exp(-alpha * n * F_CONST * (waveform - E0) / (R_CONST * TEMP)),
                             - np.exp((1 - alpha) * n * F_CONST * (waveform - E0) / (R_CONST * TEMP))])
-        current[t] = n * F_CONST * k0 * butler_volmer * concentration_surface
+        current[t] = n * F_CONST * k0 * (butler_volmer[:, t] @ concentration_surface)
 
     return [waveform, current]
+
+waveform, current = cv_sim(
+    initial=0.0,
+    switch=0.2,
+    scan_rate=0.05,
+    k0=0.01,
+    alpha=0.5,
+    E0=0.1,
+    c_ox=1.0,
+    c_red=0.0,
+    D_ox=1e-3,
+    D_red=1e-3,
+    n=1,
+)
+
+data = np.column_stack((waveform, current))
+np.savetxt(
+    "cv_output.csv",
+    data,
+    delimiter=",",
+    header="potential,current",
+    comments="",
+    fmt="%.8g",
+)
+print("结果已保存到 cv_output.csv")
