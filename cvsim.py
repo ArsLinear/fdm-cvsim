b48b26017965a737cb1c3d60a1444505470df771
ArsLinear <git@arslinear.com>
2026-09-24 07:56:06 +0800

    优化 cv_sim 函数中的 Butler-Volmer 计算逻辑，调整电流计算流程以提高性能

--- cvsim.py
+++ cvsim.py
@@ -49,10 +49,6 @@ def cv_sim(initial, switch, scan_rate, k0, alpha, E0, c_ox, c_red, D_ox, D_red, 
     diffusivity = np.array([[D_ox, 0.0], [0.0, D_red]])
     coefficient = diffusivity * dt / dx ** 2
 
-    butler_volmer = np.array([np.exp(-alpha * n * F_CONST * (waveform[t] - E0) / (R_CONST * TEMP)),
-                            - np.exp((1 - alpha) * n * F_CONST * (waveform[t] - E0) / (R_CONST * TEMP))])
-    flux = 2 * k0 * dt / dx * np.outer([1.0, -1.0], butler_volmer)
-
     big_matrix = np.zeros((num, num, 2, 2))
 
     for i in range(1, num - 1):
@@ -64,12 +60,16 @@ def cv_sim(initial, switch, scan_rate, k0, alpha, E0, c_ox, c_red, D_ox, D_red, 
 
     for t in range(len(waveform)):
 
+        butler_volmer = np.array([np.exp(-alpha * n * F_CONST * (waveform[t] - E0) / (R_CONST * TEMP)),
+                            - np.exp((1 - alpha) * n * F_CONST * (waveform[t] - E0) / (R_CONST * TEMP))])
+        flux = 2 * k0 * dt / dx * np.outer([1.0, -1.0], butler_volmer)
+
         big_matrix[0, 0] = np.eye(2) + 2 * coefficient + flux
         big_matrix[0, 1] = -2 * coefficient
 
-        big_matrix = big_matrix.transpose(0, 2, 1, 3).reshape(2 * num, 2 * num)
+        matrix_2d = big_matrix.transpose(0, 2, 1, 3).reshape(2 * num, 2 * num)
         concentration[num-1] = [c_ox, c_red]
-        concentration = spsolve(csr_matrix(big_matrix), concentration.reshape(-1)).reshape(num, 2)
+        concentration = spsolve(csr_matrix(matrix_2d), concentration.reshape(-1)).reshape(num, 2)
 
         concentration_surface = concentration[0, :]
         butler_volmer = np.array([np.exp(-alpha * n * F_CONST * (waveform[t] - E0) / (R_CONST * TEMP)),
