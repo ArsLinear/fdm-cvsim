663e2c12357d0ab0c72ad90dc7c26b68b7fd8e91
ArsLinear <git@arslinear.com>
2026-09-24 07:52:01 +0800

    更新 cvsim.py 文件，优化波形生成和电流计算逻辑，添加时间性能监测；更新 pixi.toml 文件以包含 scipy 依赖

--- cvsim.py
+++ cvsim.py
@@ -1,4 +1,7 @@
 import numpy as np
+import time
+from scipy.sparse import csr_matrix
+from scipy.sparse.linalg import spsolve
 
 SAMPLE_TIME = 0.005  # s；0.5 V/s 时每步扫描 2.5 mV
 
@@ -17,20 +20,22 @@ def cv_waveform_generator(initial, switch, scan_rate):
     t_1 = np.arange(0, (switch - initial) / scan_rate, dt)
     t_2 = np.arange((switch - initial) / scan_rate, 2 * (switch - initial) / scan_rate, dt)
 
-    time = np.abs(2 * (switch - initial) / scan_rate)
+    time_tot = np.abs(2 * (switch - initial) / scan_rate)
 
     waveform_1 = initial + scan_rate * t_1
     waveform_2 = switch - scan_rate * (t_2 - (switch - initial) / scan_rate)
 
     waveform = np.concatenate([waveform_1, waveform_2])
 
-    return waveform, time
+    return waveform, time_tot
 
 def cv_sim(initial, switch, scan_rate, k0, alpha, E0, c_ox, c_red, D_ox, D_red, n):
 
-    waveform, time = cv_waveform_generator(initial, switch, scan_rate)
+    start = time.perf_counter()
 
-    length_diff = 6 * np.sqrt(max(D_ox, D_red) * time)
+    waveform, time_tot = cv_waveform_generator(initial, switch, scan_rate)
+
+    length_diff = 6 * np.sqrt(max(D_ox, D_red) * time_tot)
 
     space_num = int(np.ceil(length_diff / SAMPLE_SPACE))
 
@@ -41,35 +46,38 @@ def cv_sim(initial, switch, scan_rate, k0, alpha, E0, c_ox, c_red, D_ox, D_red, 
     dx = SAMPLE_SPACE
     num = space_num + 1
 
+    diffusivity = np.array([[D_ox, 0.0], [0.0, D_red]])
+    coefficient = diffusivity * dt / dx ** 2
+
+    butler_volmer = np.array([np.exp(-alpha * n * F_CONST * (waveform[t] - E0) / (R_CONST * TEMP)),
+                            - np.exp((1 - alpha) * n * F_CONST * (waveform[t] - E0) / (R_CONST * TEMP))])
+    flux = 2 * k0 * dt / dx * np.outer([1.0, -1.0], butler_volmer)
+
+    big_matrix = np.zeros((num, num, 2, 2))
+
+    for i in range(1, num - 1):
+        big_matrix[i, i-1] = -coefficient
+        big_matrix[i, i] = np.eye(2) + 2 * coefficient
+        big_matrix[i, i+1] = -coefficient
+
+    big_matrix[num-1, num-1] = np.eye(2)
+
     for t in range(len(waveform)):
-
-        diffusivity = np.array([[D_ox, 0.0], [0.0, D_red]])
-        coefficient = diffusivity * dt / dx ** 2
-
-        butler_volmer = np.array([np.exp(-alpha * n * F_CONST * (waveform[t] - E0) / (R_CONST * TEMP)),
-                                - np.exp((1 - alpha) * n * F_CONST * (waveform[t] - E0) / (R_CONST * TEMP))])
-        flux = 2 * k0 * dt / dx * np.outer([1.0, -1.0], butler_volmer)
-
-        big_matrix = np.zeros((num, num, 2, 2))
 
         big_matrix[0, 0] = np.eye(2) + 2 * coefficient + flux
         big_matrix[0, 1] = -2 * coefficient
 
-        for i in range(1, num - 1):
-            big_matrix[i, i-1] = -coefficient
-            big_matrix[i, i] = np.eye(2) + 2 * coefficient
-            big_matrix[i, i+1] = -coefficient
-
-        big_matrix[num-1, num-1] = np.eye(2)
-
         big_matrix = big_matrix.transpose(0, 2, 1, 3).reshape(2 * num, 2 * num)
         concentration[num-1] = [c_ox, c_red]
-        concentration = np.linalg.solve(big_matrix, concentration.reshape(-1)).reshape(num, 2)
+        concentration = spsolve(csr_matrix(big_matrix), concentration.reshape(-1)).reshape(num, 2)
 
         concentration_surface = concentration[0, :]
         butler_volmer = np.array([np.exp(-alpha * n * F_CONST * (waveform[t] - E0) / (R_CONST * TEMP)),
                             - np.exp((1 - alpha) * n * F_CONST * (waveform[t] - E0) / (R_CONST * TEMP))])
         current[t] = n * F_CONST * k0 * (butler_volmer @ concentration_surface)
+
+    end = time.perf_counter()
+    print(f"Simulation time: {end - start:.2f} s")
 
     return [waveform, current]
 
